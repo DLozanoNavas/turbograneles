95bf4a971e0766cab0b5fff7eedba4396354ac20
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-19 14:10:11 -0500

    [Add] GetLoadByEachVesselUseCase use case interface and implementation

---
+++ src/Italcol.Turbograneles.Application/UseCases/GetLoadByEachVesselUseCase.cs
@@ -0,0 +1,16 @@
+﻿using Italcol.Turbograneles.Application.Services;
+using Italcol.Turbograneles.Application.UseCases.Interfaces;
+using Italcol.TurboGraneles.Clients.Models;
+using Microsoft.Kiota.Abstractions.Serialization;
+
+namespace Italcol.Turbograneles.Application.UseCases
+{
+    public class GetLoadByEachVesselUseCase(IPortClientFactoryService portClientFactoryService) : IGetLoadByEachVesselUseCase
+    {
+        public async Task<UntypedNode?> ExecuteAsync(GetLoadByEachVesselDto request)
+        {
+            var client = await portClientFactoryService.CreateClientAsync();
+            return await client.Api.V1.Miit.GetLoadByEachVessel.PostAsync(request);
+        }
+    }
+}

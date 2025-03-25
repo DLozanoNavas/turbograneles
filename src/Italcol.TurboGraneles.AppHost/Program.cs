fcd2fc2c15479dcf4027a3d07e6c7574639d913c
Daniel Lozano <d.lozanonavas@outlook.com>
2025-03-25 12:15:33 -0500

    Update Identity

--- src/Italcol.TurboGraneles.AppHost/Program.cs
+++ src/Italcol.TurboGraneles.AppHost/Program.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var rabbitmq = builder.AddRabbitMQ("messaging");
 
-builder.AddProject<Projects.Italcol.Turbograneles_Adapters>("Italcol.Turbograneles-adapters")
+builder.AddProject<Projects.Italcol_Turbograneles_Adapters>("WebApi")
     .WithReference(rabbitmq);
 
 builder.Build().Run();

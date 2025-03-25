f0c179137223015a0227493b533af176b183cf14
Daniel Lozano <d.lozanonavas@outlook.com>
2025-03-25 17:20:02 -0500

    Adds PG Setup and Identity endpoints

--- src/Italcol.TurboGraneles.AppHost/Program.cs
+++ src/Italcol.TurboGraneles.AppHost/Program.cs
@@ -1,8 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var rabbitmq = builder.AddRabbitMQ("messaging");
+var postgres = builder.AddPostgres("data").WithPgAdmin();
+var postgresdb = postgres.AddDatabase("postgresdb");
 
 builder.AddProject<Projects.Italcol_Turbograneles_Adapters>("WebApi")
+    .WithReference(postgresdb)
     .WithReference(rabbitmq);
 
 builder.Build().Run();
--- src/Italcol.Turbograneles.Adapters/Program.cs
+++ src/Italcol.Turbograneles.Adapters/Program.cs
@@ -1,31 +1,38 @@
+using Italcol.Turbograneles.Adapters.Data;
 using Italcol.Turbograneles.Application.Services;
 using Italcol.Turbograneles.Application.UseCases;
 using Italcol.Turbograneles.Application.UseCases.Interfaces;
 
-using Microsoft.Kiota.Abstractions.Authentication;
 using Microsoft.EntityFrameworkCore;
-using Italcol.Turbograneles.Adapters.Data;
+using Microsoft.Kiota.Abstractions.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DataContextConnection") ?? throw new InvalidOperationException("Connection string 'DataContextConnection' not found.");;
+var connectionString = builder.Configuration.GetConnectionString("postgresdb") ?? throw new InvalidOperationException("Connection string 'postgresdb' not found.");;
 
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+//builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(connectionString));
+
 
 builder.Services
-    .AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddIdentityApiEndpoints<ApplicationUser>()
     .AddEntityFrameworkStores<DataContext>();
 
+
+
 builder.AddServiceDefaults();
+
+builder.Services.AddAuthorization();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
-
-
 builder.Services.AddEndpointsApiExplorer();
+
+
+
 builder.Services.AddHttpClient<PortClientTokenFactoryService>();
 builder.Services.AddScoped<IAuthenticationProvider, BearerTokenAuthProvider>();
 builder.Services.AddScoped<IPortClientFactoryService, PortClientFactoryService>();
@@ -54,6 +61,7 @@ app.UseSwaggerUI(static options =>
     options.SwaggerEndpoint("/openapi/v1.json", "My API V1");
 });
 
+app.MapGroup("/account").MapIdentityApi<ApplicationUser>();
 
 app.MapControllers();
 app.UseHttpsRedirection();

63bad29f3b6b966744fef02d60dc949c4da2c8be
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-26 09:20:54 -0500

    Add new PartialShipments model

---
+++ src/Italcol.Turbograneles.Domain/Metalteco/PartialShipment.cs
@@ -0,0 +1,11 @@
+﻿namespace Italcol.Turbograneles.Domain.Metalteco
+{
+    public class PartialShipment
+    {
+        public int TransactionId { get; set; }
+        public int ProductId { get; set; }
+        public long ReceiptWeight { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}

582c6600d156085cda830069bb16792e05b67bbe
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-28 13:32:59 -0500

    Initial creation for domain models, subject to changes

---
+++ src/Italcol.Turbograneles.Domain/Truck.cs
@@ -0,0 +1,8 @@
+﻿namespace Italcol.Turbograneles.Domain
+{
+    public class Truck
+    {
+        public string Plate { get; set; }
+        public List<TruckAppointment> Appointments { get; set; }
+    }
+}
